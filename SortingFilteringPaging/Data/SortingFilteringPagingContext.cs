ccf50cbca0b2da58021e0ecacc3f6f794ffd6d3a
Valentas Klimas <valentasklimas@gmail.com>
2020-06-03 14:15:44 +0100

    New DbContext Class. Added Controller, Views,

---
+++ SortingFilteringPaging/Data/SortingFilteringPagingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SortingFilteringPaging.Models;
+
+namespace SortingFilteringPaging.Data
+{
+    public class SortingFilteringPagingContext : DbContext
+    {
+        public SortingFilteringPagingContext (DbContextOptions<SortingFilteringPagingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
