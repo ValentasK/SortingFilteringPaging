90533bf36d64c590e6515ed404d6716618093fe4
Valentas Klimas <valentasklimas@gmail.com>
2020-06-03 13:56:08 +0100

    Db context class added

---
+++ SortingFilteringPaging/Data/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SortingFilteringPaging.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+    }
+}
