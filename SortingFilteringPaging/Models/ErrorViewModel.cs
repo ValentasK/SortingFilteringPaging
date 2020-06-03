265b3af43d68a2d4428ba5db06a7d7c66926be2b
Valentas Klimas <valentasklimas@gmail.com>
2020-06-03 13:51:24 +0100

    Add project files.

---
+++ SortingFilteringPaging/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SortingFilteringPaging.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
