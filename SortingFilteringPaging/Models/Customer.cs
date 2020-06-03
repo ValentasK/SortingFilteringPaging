5b8f0d4869c59b84ced9fa1f4c2d6468bf747ee5
Valentas Klimas <valentasklimas@gmail.com>
2020-06-03 14:10:03 +0100

    Customer model

---
+++ SortingFilteringPaging/Models/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SortingFilteringPaging.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string  FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(6)]
+        public string MaleFemale { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public int PhoneNumber { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string EmailAddress { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Street { get; set; }
+        [Required]
+        [MaxLength(5)]
+        public string HouseNr { get; set; }
+    }
+}
