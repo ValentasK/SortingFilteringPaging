a37f6bf60065ea430c1525f4b228c54d0c7e37b7
Valentas Klimas <valentasklimas@gmail.com>
2020-06-11 10:09:00 +0100

    Records numeration and comments added

--- SortingFilteringPaging/Controllers/CustomersController.cs
+++ SortingFilteringPaging/Controllers/CustomersController.cs
@@ -23,19 +23,19 @@ namespace SortingFilteringPaging.Controllers
         // GET: Customers
         public async Task<IActionResult> Index(string SearchString,int Page, string Paging, string Sorting, int ItemsInPage = 25, int SkipValue = 0 )
         {
-
+            //Function to generate random "Customer" objects
             //SeedData sd = new SeedData();
             //_context.AddRange(sd.GeneratedCustomers(100259));
             //_context.SaveChanges();
             
-            int skip = SkipValue;
+            int skip = SkipValue; // indicates how many records to skip from Database
             int totalRecords = _context.Customer.Count(); // get the number of all the records
-            bool isItLastPage = false;
-            bool isItFirstPage = false;
+            bool isItLastPage = false; // indicates is the data is from first page
+            bool isItFirstPage = false; // indicates is the data is from last page
 
-            if (!String.IsNullOrEmpty(Paging))
+            if (!String.IsNullOrEmpty(Paging)) // increase or decrease skip value
             {              
-                    if (Paging == "Next")
+                    if (Paging == "Next") 
                     {
                         skip = skip + ItemsInPage;
                     }
@@ -48,19 +48,20 @@ namespace SortingFilteringPaging.Controllers
                     }                     
             }
 
-            if (Page != 0)
+            if (Page != 0)  // if page number was received it will calculate new skip value according to page
             {
                 Page = Page - 1;
                 skip = Page * ItemsInPage;
             }
 
-            List<Customer> customers = new List<Customer>(); // create new list of customers          
+            List<Customer> customers = new List<Customer>(); // create empty new list of the customers          
 
-            if (!String.IsNullOrEmpty(SearchString))
+            if (!String.IsNullOrEmpty(SearchString)) // if search string was received 
             {
-                if (!String.IsNullOrEmpty(Sorting))
+                if (!String.IsNullOrEmpty(Sorting)) // if sorting value was received: "by name, age, surname etc..."
                 {
-                    customers = _context.Customer.Where(x =>
+                    // LINQ which will be sent to database
+                    customers = _context.Customer.Where(x => 
                     x.FirstName.ToLower().Contains(SearchString.ToLower()) ||
                     x.LastName.ToLower().Contains(SearchString.ToLower()) ||
                    x.MaleFemale.ToLower().Contains(SearchString.ToLower()) ||
@@ -69,8 +70,8 @@ namespace SortingFilteringPaging.Controllers
                    x.EmailAddress.ToLower().Contains(SearchString.ToLower()) ||
                    x.City.ToLower().Contains(SearchString.ToLower()) ||
                    x.Street.ToLower().Contains(SearchString.ToLower()) ||
-                   x.HouseNr.ToLower().Contains(SearchString.ToLower())).OrderBy(Sorting)
-                   .Skip(skip).Take(ItemsInPage).ToList();
+                   x.HouseNr.ToLower().Contains(SearchString.ToLower())).OrderBy(Sorting) // OrderBy("Sorting") accepts String value  
+                   .Skip(skip).Take(ItemsInPage).ToList();                              // because of System.Linq.Dynamic.Core;
                 }
                 else
                 {
@@ -102,19 +103,19 @@ namespace SortingFilteringPaging.Controllers
             {
                 if (!String.IsNullOrEmpty(Sorting))
                 {
-                    customers = _context.Customer.OrderBy(Sorting)
+                    customers = _context.Customer.OrderBy(Sorting) // returns all the records from database sorted
                    .Skip(skip).Take(ItemsInPage).ToList();
                 }
                 else
                 {
-                    customers = _context.Customer.Skip(skip).Take(ItemsInPage).ToList(); // takes first 20 records 
+                    customers = _context.Customer.Skip(skip).Take(ItemsInPage).ToList(); // takes first 25 records if no skip value is reveiced
                 }             
             }
 
-            isItLastPage = totalRecords <= ItemsInPage + skip ;
-            isItFirstPage = skip == 0;
+            isItLastPage = totalRecords <= ItemsInPage + skip ; // indicates the last page
+            isItFirstPage = skip == 0; // indicates the first page
 
-            CustomerViewModel cvm = new CustomerViewModel()
+            CustomerViewModel cvm = new CustomerViewModel()  // customer view model to be sent to view 
             {
                 SkipValue = skip,
                 ItemsPerPage = ItemsInPage,
@@ -126,25 +127,7 @@ namespace SortingFilteringPaging.Controllers
                 FirsPage = isItFirstPage
             };
 
-            return View(cvm);
-        }
-
-        // GET: Customers/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var customer = await _context.Customer
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-
-            return View(customer);
+            return View(cvm); // view model sent to the view
         }
 
         // GET: Customers/Create
@@ -168,6 +151,30 @@ namespace SortingFilteringPaging.Controllers
             }
             return View(customer);
         }
+
+
+        /////////                No other actions from the below were used in this project    ////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
 
         // GET: Customers/Edit/5
         public async Task<IActionResult> Edit(int? id)
--- SortingFilteringPaging/Models/CustomerViewModel.cs
+++ SortingFilteringPaging/Models/CustomerViewModel.cs
@@ -7,14 +7,15 @@ namespace SortingFilteringPaging.Models
 {
     public class CustomerViewModel
     {
-        public int SkipValue { get; set; }
-        public int ItemsPerPage { get; set; }
-        public string SearchString { get; set; }
-        public string Sorting { get; set; }
-        public int AllRecords { get; set; }
-        public bool LastPage { get; set; }
-        public bool FirsPage { get; set; }
+        public int SkipValue { get; set; } // indicates how many records has to be skipped from the database
+        public int ItemsPerPage { get; set; } // indicates how many records have to be taken from database
+        public string SearchString { get; set; } // search string 
+        public string Sorting { get; set; } // value could be: by name, surname, age, atc...
+        public int AllRecords { get; set; } // indicates how many records are in database, if search String is applied
+                                            // then it will indicate how many records matched that search String 
+        public bool LastPage { get; set; } // if true - it is last page
+        public bool FirsPage { get; set; } // if true = it is first page
 
-        public List<Customer> Customers { get; set; }
+        public List<Customer> Customers { get; set; } // list of records to be sent to 
     }
 }
